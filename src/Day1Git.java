334b88845430094480de70d5d5f195f77aea23e1
ilkerVefa <ilkervefaedisan@gmail.com>
2022-08-16 22:04:04 +0300

    yeni

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -3,6 +3,7 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Git 1. Deneme");
         System.out.println("Hello 2. deneme");
+        System.out.println("Hello 3. deneme");
 
 
 
